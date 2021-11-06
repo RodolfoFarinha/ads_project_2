162a36ff46e7c7e8ad4a5317ad25c86c6457a548
RodolfoFarinha <rodolfo.alves.farinha@gmail.com>
2021-11-06 01:28:00 +0000

    angular repair

---
+++ App/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarComponent } from './components/calendar/calendar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- README.md
+++ README.md
@@ -13,3 +13,6 @@ App project was developed in Angular 9.1.15
 
 ssh -i ./ads_key.pem ubuntu@adsapi.menu-ai.com
 
+ng add angular-calendar
+ng add @ng-bootstrap/ng-bootstrap
+
